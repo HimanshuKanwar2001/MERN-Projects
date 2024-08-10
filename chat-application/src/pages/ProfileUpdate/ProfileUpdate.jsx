10a8aff33fba57e5d5c80fcf3971e140d93bfd9e
himanshu kanwar <himanshukanwar2001@gmail.com>
2024-08-10 17:49:46 +0530

    created login page

---
+++ chat-application/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+      we are in Profile Update pages
+    </div>
+  )
+}
+
+export default ProfileUpdate
